d410c33c0b3f553414e9476d7d5d373ac934dcb4
jimmy <najimmy@gmail.com>
2025-04-29 17:08:08 +0900

    Use pipe and etc ...

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,21 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get(":id/sub/:id2")
+  getSubRoute(
+    @Param("id", ParseIntPipe) id: number,
+    @Param("id2") id2: string
+  ): string {
+    return `sub id:${id} id2: ${id2}`;
+  }
+
+  @Get("query")
+  getQuery(
+    @Query("id") id: string,
+    @Query("id2") id2: string
+  ): string {
+    return `query id:${id} id2: ${id2}`;
   }
 }
