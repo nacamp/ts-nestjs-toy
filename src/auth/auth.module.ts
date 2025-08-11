d4a1ac71b72698169330079759cf64057e9253ce
jimmy <najimmy@gmail.com>
2025-08-11 12:23:31 +0900

    Use env for jwt configuration

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './jwt.strategy';
@@ -12,9 +13,12 @@ import { APP_GUARD } from '@nestjs/core';
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
-      secret: 'SECRET_KEY', // 실제 운영에선 process.env.JWT_SECRET
-      signOptions: { expiresIn: '1h' }, // 토큰 만료 1시간
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: config.get<string>('JWT_EXPIRES_IN') },
+      }),
     }),
   ],
   controllers: [AuthController],
